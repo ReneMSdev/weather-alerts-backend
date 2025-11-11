c8dbcd8143caadbcaef1a0a382d78eab461fde22
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-11-11 13:24:48 -0600

    Prepare for deployment

--- src/db/client.ts
+++ src/db/client.ts
@@ -4,6 +4,7 @@ import { drizzle } from 'drizzle-orm/node-postgres'
 // create a PostgreSQl connection pool
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 })
 
 // create a Drizzle client
